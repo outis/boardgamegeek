a12f61ebd27d4121a28995b9f227d1fa68d482f3
Galen Wright-Watson <ww.galen@gmail.com>
2022-01-17 13:07:59 -0800

    add: pytest.ini

--- test/test_utils.py
+++ test/test_utils.py
@@ -160,6 +160,7 @@ def test_serialization():
     assert type(dummy_unserialized) == Thing
 
 
+@pytest.mark.slow
 def test_rate_limiting_for_requests():
     # create two threads, give each a list of games to fetch, disable cache and time the amount needed to
     # fetch the data. requests should be serialized, even if made from two different threads
